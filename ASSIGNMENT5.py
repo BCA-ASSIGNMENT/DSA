061df0d699fe99e6bc6bd82f02a20e247521eebf
HARAJIT DAS <110291590+HARAJIT05@users.noreply.github.com>
2023-03-13 11:58:50 +0530

    Create ASSIGNMENT5.py

---
+++ ASSIGNMENT5.py
@@ -0,0 +1,19 @@
+######################################
+#IMPLEMENTATION OF BINARY SEARCH
+######################################
+x = [2, 4, 10, 18, 20, 22, 32, 34]
+y = int(input("Enter a number: "))
+low = 0
+high = len(x) - 1
+while low <= high:
+    mid = (low + high) // 2  
+    if x[mid] == y:  
+        print("Found y at index", mid)
+        break
+    if x[mid] < y:
+        low = mid + 1
+    elif x[mid] > y:
+        high = mid - 1
+else:
+    print("y not found")
+    
