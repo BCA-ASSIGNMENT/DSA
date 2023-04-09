d5651610d1e7022d3b9b94481712f8bd19a8328d
HARAJIT DAS <110291590+HARAJIT05@users.noreply.github.com>
2023-04-09 21:34:58 +0530

    Update double linklist.py

--- double linklist.py
+++ double linklist.py
@@ -1,6 +1,6 @@
-##################################################################
-#IMPLEMENTATION FD DOUBLE LINK LIST IN PYTHON
-##################################################################
+######################################################
+#IMPLEMENTATION OF DOUBLE LINKED LIST IN PYTHON
+######################################################
 class Node:
     def __init__(self, data):
         self.data = data
@@ -27,34 +27,9 @@ class DoubleLinkList:
         while self.last is not None:
             print(self.last.data)
             self.last=self.last.prev
-    def delete_at_last(self):
-        if self.head != None:
-            self.last.prev.next = None
-            self.last = self.last.prev
-        else:
-            print("list is empty")
-    def insert_at_pos(self, n, pos):
-        newnode = Node(n)
-        temp = self.head
-        for i in range(pos-1):
-            temp = temp.next
-        newnode.next = temp.next
-        temp.next = newnode
-        newnode.prev = temp
-        newnode.next.prev = newnode
-    def delete_at_front(self):
-        if self.head is None:
-            return None
-        else:
-            temp = self.head
-            self.head = self.head.next
-            return temp.data
 l=DoubleLinkList()
 l.createlist(1)
 l.createlist(2)
 l.createlist(3)
 l.createlist(4)
-l.insert_at_pos(8, 2)
-l.delete_at_last()
-l.delete_at_front()
 l.printlist()
